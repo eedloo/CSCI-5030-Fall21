4faa5bccdeb087ccec15fd44017d318d5a897a90
rezarz98 <93609338+rezarz98@users.noreply.github.com>
2021-11-30 22:44:32 -0600

    Add files via upload

---
+++ inputActions.py
@@ -0,0 +1,9 @@
+from action import action
+class Input_Actions:
+    def __init__(self, actions):
+        self.actions = actions
+
+        for act in self.actions:
+            if(act != None):
+                action(act)
+                print(act)
