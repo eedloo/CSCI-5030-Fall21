98a927216dcbbe95de84a5cd27697f88be4aa2d2
rezarz98 <93609338+rezarz98@users.noreply.github.com>
2021-11-30 22:42:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from action import action
-from menu import show_menu
+from menu import Menu
+from inputActions import Input_Actions
+
 
 def run():
-    show_menu()
-    
+    sample_code = [
+        'CreateCharacter(Tom, D)',
+        'SetExpression(Tom, Happy)',
+        'SetClothing(Tom, Merchant)',
+        'SetHairStyle(Tom, mage_beard)',
+        'CreatePlace(SpookyPath, SpookyPath)'
+        'SetPosition(Tom, SpookyPath.WestEnd)',
+        'SetCameraFocus(Tom)',
+    ]
+
+    menu = Menu()
+    input_action = Input_Actions(sample_code)
+
+    menu.show_menu()
+
+run()
