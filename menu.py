fb65130fafd595af9ea401fb6625db2aca9dc862
rezarz98 <93609338+rezarz98@users.noreply.github.com>
2021-11-30 22:41:44 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,20 +1,22 @@
 from action import action
 
-def wait_for(msg):
-    while True:
-        receivedmsg = input()
-        if receivedmsg == msg:
-            return True
-        elif receivedmsg == 'input Selected Credits':
-            action('SetCredits(Reza!)')
-            action('ShowCredits()')
-        elif receivedmsg == 'input Selected Quit':
-            action('Quit()')
-        elif receivedmsg == 'input Close Credits':
-            action('HideCredits()')
 
+class Menu:
+    def wait_for(self, msg):
+        while True:
+            receivedmsg = input()
+            if receivedmsg == msg:
+                return True
+            elif receivedmsg == 'input Selected Credits':
+                action('SetCredits(This is a project for Principle of Software Development Class that is done by '
+                       'Reza, Mohsen, Satish and Anjani.)')
+                action('ShowCredits()')
+            elif receivedmsg == 'input Selected Quit':
+                action('Quit()')
+            elif receivedmsg == 'input Close Credits':
+                action('HideCredits()')
 
-def show_menu():
-    action ('ShowMenu()')
-    wait_for('input Selected Start')
-    action('HideMenu()')
+    def show_menu(self):
+        action('ShowMenu()')
+        self.wait_for('input Selected Start')
+        action('HideMenu()')
