11960a38ef24b150e027a6c497fae5b0448b199f
rezarz98 <93609338+rezarz98@users.noreply.github.com>
2021-12-02 13:26:26 -0600

    Update action.py

--- action.py
+++ action.py
@@ -1,16 +1,18 @@
+# Interacting with Camelot by using input commands
 
-#Interating with Camelot by unsing input commands
-def action(input_command):
-    print('start ' + input_command)
-    return check_success(input_command)
+class Action:
 
-#Check with the Camelot whther the command succeeded or failed
-def check_success(input_command):
-    while True:
-        received = input()
-        if received == 'succeeded ' + input_command:
-            return True
-        elif received.startswith('failed ' + input_command):
-            return False
-        elif received.startswith('error ' + input_command):
-            return False
+    def action(self, input_command):
+        print('start ' + input_command)
+        #return check_success(input_command)
+
+    # Check with the Camelot whether the command succeeded or failed
+    def check_success(self, input_command):
+        while True:
+            received = input()
+            if received == 'succeeded ' + input_command:
+                return True
+            elif received.startswith('failed ' + input_command):
+                return False
+            elif received.startswith('error ' + input_command):
+                return False
