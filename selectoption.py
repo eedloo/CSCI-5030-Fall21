9fa0527009a12b69551e62a1b15407fcf16c1aab
Satish0404 <93215333+Satish0404@users.noreply.github.com>
2021-12-06 12:15:46 -0600

    Delete selectoption.py

--- selectoption.py
+++
@@ -1,6 +0,0 @@
-from playerchoice import ShowOption
-class GetSelectedOption(Showoptions):
-   def get_selected_option(slef):
-       comm = Communication()
-       selected_option = comm.get_input_from_player()
-       return selected_option
