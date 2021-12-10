55d1c90dfe11368db9231c8b6b4ad8dc980d00ff
anjani1998 <93215619+anjani1998@users.noreply.github.com>
2021-12-10 11:00:46 -0600

    Update playerchoice.py

--- playerchoice.py
+++ playerchoice.py
@@ -5,20 +5,23 @@ from Communicator import Communicator
 from inputActions import Input_Actions
 from disableicon import DisableIcon
 
+
 class ShowOptions(DisableIcon):
-    def init(self, opt1, opt2):
+    def __init__(self, opt1, opt2):
         self.opt1 = opt1
         self.opt2 = opt2
 
     def show_options(self):
         inputs = [
-            'EnableInput'
-            'EnableIcon(' + self.opt1 + ', ' + self.opt1.islower() + ', DiningRoom.Door, "Go to" ' + self.opt1.islower() + '" )'
-            'EnableIcon(' + self.opt1 + ', ' + self.opt1.islower() + ', DiningRoom.BackDoor, "Go to" ' + self.opt1.islower() + '" )'
-            'EnableIcon(' + self.opt2 + ', ' + self.opt2.islower() + ', DiningRoom.Door, "Go to" ' + self.opt2.islower() + '" )'
-            'EnableIcon(' + self.opt2 + ', ' + self.opt2.islower() + ', DiningRoom.BackDoor, "Go to" ' + self.opt2.islower() + '" )'
+            'EnableInput',
+            'EnableIcon(' + self.opt1 + ', ' + str(self.opt1.lower()) + ', DiningRoom.Door, "Go to " ' + self.opt1 + ' )',
+            'EnableIcon(' + self.opt1 + ', ' + str(self.opt1.lower()) + ', DiningRoom.BackDoor, "Go to" ' + self.opt1 + '" )',
+            'EnableIcon(' + self.opt2 + ', ' + str(self.opt2.lower())+ ', DiningRoom.Door, "Go to" ' + self.opt2+ '" )',
+            'EnableIcon(' + self.opt2 + ', ' + str(self.opt2.lower()) + ', DiningRoom.BackDoor, "Go to" ' + self.opt2 + '" )'
         ]
-        input_acts = Input_Actions(inputs)
+
+        input_acts = Input_Actions()
+        input_acts.get_actions(inputs)
 
     def get_selected_option(self):
         comm = Communicator()
@@ -32,13 +35,3 @@ class ShowOptions(DisableIcon):
             return 'WalkTo(Tom, DiningRoom.Door)'
         else:
             return 'WalkTo(Tom, DiningRoom.BackDoor)'
-
- 
- 
-#testing player choice, enable_icon and disable_icon
-#opt1 = "Forest"
-#opt2 = "Bookshelf"
-#show_opt = ShowOptions(opt1, opt2)
-#show_opt.show_options()
-#selected_option = show_opt.get_selected_option()
-#show_opt.disable_icon(selected_option)
