2733e8c2ce4c463c56d1860b5b608c384383388e
anjani1998 <93215619+anjani1998@users.noreply.github.com>
2021-12-10 11:05:05 -0600

    Update disableicon.py

--- disableicon.py
+++ disableicon.py
@@ -1,13 +1,17 @@
 ####### PSD Project #######
 ##### Satish ####
+from inputActions import Input_Actions
+
 
 class DisableIcon:
-   def disable_icon(self, option):
-       if option == self.opt1:
-           inputs = [ 'DisableIcon(' + self.opt1 + ', Tom )' ]
-           input_acts = Input_Actions(inputs)
-           menu.show_menu()
+    def disable_icon(self, option):
+        input_acts = Input_Actions()
+        if option == self.opt1:
+            inputs = ['DisableIcon(' + self.opt1 + ', DiningRoom.Door)']
+            inputs = ['DisableIcon(' + self.opt1 + ', DiningRoom.BackDoor)']
+            input_acts.get_actions(inputs)
+
         else:
-           inputs = [ 'DisableIcon(' + self.opt2 + ', Tom )' ]
-           input_acts = Input_Actions(inputs)
-           menu.show_menu()
+            inputs = ['DisableIcon(' + self.opt2 + ', DiningRoom.Door)']
+            inputs = ['DisableIcon(' + self.opt2 + ', DiningRoom.BackDoor)']
+            input_acts.get_actions(inputs)
