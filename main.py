05ce2186ed85aa646d9ced76999a442003aa987f
anjani1998 <93215619+anjani1998@users.noreply.github.com>
2021-12-10 11:02:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-from menu import Menu
+from Place import Place
+from Char import Char
 from inputActions import Input_Actions
+from ActionFunctions import Perform
+from playerchoice import ShowOptions
+from menu import Menu
+from disableicon import DisableIcon
+
+#Show Menu
+menu = Menu()
+menu.show_menu()
+
+input_act = Input_Actions()
+perform = Perform()
+
+#create DiningRoom
+place = Place()
+dining_room=place.create_place('DiningRoom', 'DiningRoom')
+input_act.get_actions(dining_room)
+
+#create character Tom
+tom=Char()
+char_tom = (tom.create_char('Tom', 'D', 'Merchant', 'Short', 'Black', 'DiningRoom'))
+input_act.get_actions(char_tom)
+
+#SetCamera
+input_act.get_actions(perform.set_camera('Tom'))
+
+#Enable Icon
+choice = ShowOptions("Bookshelf", "Forest")
+choice.show_options()
+
+#Disable Icon
+
+disable = DisableIcon()
+
+#Enable Input
+input_act.get_actions(perform.enable_input())
 
 
-def run():
-    sample_code = [
-        'CreateCharacter(Tom, D)',
-        'SetExpression(Tom, Happy)',
-        'SetClothing(Tom, Merchant)',
-        'SetHairStyle(Tom, mage_beard)',
-        'CreatePlace(SpookyPath, SpookyPath)'
-        'SetPosition(Tom, SpookyPath.WestEnd)',
-        'SetCameraFocus(Tom)',
-    ]
+while (True):
+    input()
 
-    menu = Menu()
-    input_action = Input_Actions(sample_code)
 
-    menu.show_menu()
-
-run()
